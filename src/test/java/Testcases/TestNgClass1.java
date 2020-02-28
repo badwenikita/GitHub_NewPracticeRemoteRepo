5be56af69cf529f71994916d388d6ddd942510b4
badwenikita <58299244+badwenikita@users.noreply.github.com>
2020-02-28 15:26:36 +0530

    method2 added in childBranch

--- src/test/java/Testcases/TestNgClass1.java
+++ src/test/java/Testcases/TestNgClass1.java
@@ -8,4 +8,11 @@ public class TestNgClass1 {
   {
 	  System.out.println("method added from Eclipse tool");
   }
+	
+  @Test
+  public void method2() 
+  {
+	  System.out.println("method2 added only in ChildBranch");
+  }
+	
 }
