fdf7f2a4d1d2fe674f6ee78eb7cab1fd1b21bbe6
badwenikita <badwe.nikita@gmail.com>
2020-02-28 15:22:45 +0530

    initial commit

---
+++ src/test/java/Testcases/TestNgClass1.java
@@ -0,0 +1,11 @@
+package Testcases;
+
+import org.testng.annotations.Test;
+
+public class TestNgClass1 {
+  @Test
+  public void method1() 
+  {
+	  System.out.println("method added from Eclipse tool");
+  }
+}
